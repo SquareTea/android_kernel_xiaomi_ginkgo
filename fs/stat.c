755088debb6d7ecffd966a05b139f9f0d1c151b6
SquareTea <164426076+SquareTea@users.noreply.github.com>
2025-12-17 18:26:20 +0700

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -161,8 +161,6 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 	return error;
 }
 EXPORT_SYMBOL(vfs_statx_fd);
-/* KSU Stuff */
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 
 /**
  * vfs_statx - Get basic and extra attributes by filename
@@ -185,8 +183,6 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-/* KSU stuff */    
-    ksu_handle_stat(&dfd, &filename, &flags);
 	
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
